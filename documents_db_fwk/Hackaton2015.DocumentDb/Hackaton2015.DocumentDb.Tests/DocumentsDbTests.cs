94ce14fc0cbdb93a149ab9872660d6a726b7ce7b
Gabriel <gabriel@kinetica-solutions.com>
2015-01-28 10:33:01 -0300

    creo proyecto de Test

---
+++ documents_db_fwk/Hackaton2015.DocumentDb/Hackaton2015.DocumentDb.Tests/DocumentsDbTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hackaton2015.DocumentDb.Tests
+{
+    [TestClass]
+    public class DocumentsDbTests
+    {
+        [TestMethod]
+        public void TestDocumentDbConnection()
+        {
+        }
+    }
+}
