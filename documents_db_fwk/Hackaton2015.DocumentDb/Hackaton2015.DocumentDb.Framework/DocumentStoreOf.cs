71a65702b829d463d6cc324811912bd90c192431
Gabriel <gabriel@kinetica-solutions.com>
2015-01-28 15:50:56 -0300

    organizando namespaces

---
+++ documents_db_fwk/Hackaton2015.DocumentDb/Hackaton2015.DocumentDb.Framework/DocumentStoreOf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Azure.Documents.Client;
+
+namespace Hackaton2015.DocumentDb.Framework
+{
+    public class DocumentStoreOf<T>
+    {
+        private DocumentClient _client;
+
+        public DocumentStoreOf()
+        {
+            try
+            {
+                var configSection = (AzureDocumentDbConfigSection)ConfigurationManager.GetSection("azureDocumentDbConfigGroup/azureDocumentDbConfig");
+                var endpointUrl = configSection.Connection.EndpointUrl;
+                var key = configSection.Connection.Key;
+                _client = new DocumentClient(new Uri(endpointUrl), key);
+            }
+            catch (Exception)
+            {
+                throw new Exception("AzureDbDocumentFramework configuration not found");
+            }
+        }
+    }
+}
--- documents_db_fwk/Hackaton2015.DocumentDb/Hackaton2015.DocumentDb.Tests/ApiDesignTests.cs
+++ documents_db_fwk/Hackaton2015.DocumentDb/Hackaton2015.DocumentDb.Tests/ApiDesignTests.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hackaton2015.DocumentDb.Framework;
-using Microsoft.Azure.Documents.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Hackaton2015.DocumentDb.Tests
@@ -29,26 +26,6 @@ namespace Hackaton2015.DocumentDb.Tests
             //                      settings.SortAscending, 
             //                      (pageNumber - 1) * settings.ItemsPerPage, 
             //                      settings.ItemsPerPage);
-        }
-    }
-
-    public class DocumentStoreOf<T>
-    {
-        private DocumentClient _client;
-
-        public DocumentStoreOf()
-        {
-            try
-            {
-                var configSection = (AzureDocumentDbConfigSection)ConfigurationManager.GetSection("azureDocumentDbConfigGroup/azureDocumentDbConfig");
-                var endpointUrl = configSection.Connection.EndpointUrl;
-                var key = configSection.Connection.Key;
-                _client = new DocumentClient(new Uri(endpointUrl), key);
-            }
-            catch (Exception)
-            {
-                throw new Exception("AzureDbDocumentFramework configuration not found");
-            }
         }
     }
 
