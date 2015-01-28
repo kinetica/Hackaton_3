606e3f4d6dd2bf9fa44df8e177176dbadfaa607d
Gabriel <gabriel@kinetica-solutions.com>
2015-01-28 10:49:07 -0300

    primer test de conexión

--- documents_db_fwk/Hackaton2015.DocumentDb/Hackaton2015.DocumentDb.Tests/DocumentsDbTests.cs
+++ documents_db_fwk/Hackaton2015.DocumentDb/Hackaton2015.DocumentDb.Tests/DocumentsDbTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Hackaton2015.DocumentDb.Tests
@@ -9,6 +10,9 @@ namespace Hackaton2015.DocumentDb.Tests
         [TestMethod]
         public void TestDocumentDbConnection()
         {
+            var endpointUrl = "https://loggingdb.documents.azure.com:443/";
+            var authorizationKey = "3wsBb/bezEWl07MKuccaCUu4lvOL3JgduPl2ZTFcWqr0Lxj/WTvXTkGtWq8P7C4YdOmZxbh9BnSnxIJIK0L1iA==";
+            var client = new DocumentClient(new Uri(endpointUrl), authorizationKey);
         }
     }
 }
