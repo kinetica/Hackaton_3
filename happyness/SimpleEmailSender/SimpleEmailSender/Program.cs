d6e8a7f48b2e339f00f7fccf4710c352d0fd50d4
matiasiacono <matias@kinetica-solutions.com>
2015-01-28 12:43:58 -0300

    .

--- happyness/SimpleEmailSender/SimpleEmailSender/Program.cs
+++ happyness/SimpleEmailSender/SimpleEmailSender/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -16,25 +17,30 @@ namespace SimpleEmailSender
             var emailTo = ConfigurationManager.AppSettings["to"];
             var emailFrom = ConfigurationManager.AppSettings["from"];
             var emailSubject = ConfigurationManager.AppSettings["subject"];
-            var emailBody = ConfigurationManager.AppSettings["body"];
+            var emailBodyPath = ConfigurationManager.AppSettings["body"];
+
+            var bodyContent = File.OpenText(emailBodyPath).ReadToEnd();
+
             var emailUsername = ConfigurationManager.AppSettings["username"];
             var emailPassword = ConfigurationManager.AppSettings["password"];
 
             Console.WriteLine("Mail To: " + emailTo);
             Console.WriteLine("Mail From: " + emailFrom);
             Console.WriteLine("Subject: " + emailSubject);
-            Console.WriteLine("Your Message\n" + emailBody);
+            Console.WriteLine("Your Message\n" + bodyContent);
 
             MailAddress to = new MailAddress(emailTo);
             MailAddress from = new MailAddress(emailFrom);
             MailMessage mail = new MailMessage(from, to);
             mail.Subject = emailSubject;
-            mail.Body = emailBody;
+            mail.Body = bodyContent;
+            mail.IsBodyHtml = true;
+            mail.BodyEncoding = new UTF8Encoding();
+            mail.BodyTransferEncoding = System.Net.Mime.TransferEncoding.SevenBit;
 
             SmtpClient smtp = new SmtpClient();
             smtp.Host = "smtp.gmail.com";
             smtp.Port = 587;
-
             smtp.Credentials = new NetworkCredential(emailUsername, emailPassword);
             smtp.EnableSsl = true;
 
