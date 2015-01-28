63978cbfb22bc973ee6dadec120150539966e0c7
Juan Alejandro Fausd <juan.fausd@gmail.com>
2015-01-28 17:40:15 -0300

    Added interaction with mongodb

--- happyness/backend/app.js
+++ happyness/backend/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var stormpath = require('express-stormpath');
+var consolidate = require('consolidate'); // Templating library adapter for Express
+var dbClient = require('mongodb').MongoClient;
 
 var app = express();
 
-app.set('views', './views');
-app.set('view engine', 'jade');
+app.engine('html', consolidate.swig);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 
 var stormpathMiddleware = stormpath.init(app, {
   apiKeyFile: './resources/apiKey-2MBU8QFE9GAZZGNURZ47DSC7K.properties',
@@ -16,12 +19,34 @@ var stormpathMiddleware = stormpath.init(app, {
 
 app.use(stormpathMiddleware);
 
-app.get('/', function(req, res) {
-  res.render('home', {
-    title: 'Welcome'
-  });
+dbClient.connect('mongodb://localhost:27017/happiness', function(err, db) {
+    if(err) throw err;
+
+    app.get('/', function(req, res) {
+		db.collection('votos').find().toArray(function(err, docs) {
+            if(err) throw err;
+			
+			var votos = [];
+
+            if (docs) {
+				votos = docs;
+            }
+			
+			var cantVotos = votos.length;
+			for(var i=0; i<cantVotos; i++) {
+				var puntaje = votos[i].puntaje;
+				votos[i].puntaje = [];
+				for(var j=0; j<puntaje; j++) {
+					votos[i].puntaje.push(j);
+				}
+			}
+
+			res.render('index', { votos: votos });
+        });
+	});
+
+	//app.use('/profile', require('./profile')());
+
+	app.listen(3000);
 });
 
-app.use('/profile', require('./profile')());
-
-app.listen(3000);
