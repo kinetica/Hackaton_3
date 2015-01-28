44b5ddef4911be7d24030d74e1197767b0ae74a1
Leonardo Micheloni <leomicheloni@hotmail.com>
2015-01-28 11:14:22 -0300

    works!

--- more_than_words/js/app.js
+++ more_than_words/js/app.js
@@ -1,11 +1,55 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+var languages = [{
+		id : '1',
+		name : 'English',
+		words : [{
+				id : '1',
+				word : 'dog',
+				translation : 'perro'
+			},{
+				id : '2',
+				word : 'cat',
+				translation : 'gato'
+			},{
+				id : '3',
+				word : 'bird',
+				translation : 'pájaro'
+			}
+		]
+	}
+
+];
+
+App.Router.map(function () {
+	// put your routes here
+	
+	this.resource('languages', function(){
+		this.resource('language');			
+	});
+});
+
+App.LanguagesRoute = Ember.Route.extend({
+	model: function(){
+		return languages;
+	}
+});
+
+App.LanguageRoute = Ember.Route.extend({
+	model: function(){
+		return '';
+	}
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
+		model : function () {
+			return ['red', 'yellow', 'blue'];
+		}
+	});
+
+// Idiomas tienen palabras
+// las palabras tienen una traducción
+
+// inglés => palabras => traducción
+
+// castellano
