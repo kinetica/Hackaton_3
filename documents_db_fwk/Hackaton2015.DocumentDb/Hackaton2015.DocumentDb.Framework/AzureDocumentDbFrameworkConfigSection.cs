deecf777dfe3aa89c73660d6cf16b3879acf7c04
Gabriel <gabriel@kinetica-solutions.com>
2015-01-28 15:42:42 -0300

    TDD para la api del fwk

---
+++ documents_db_fwk/Hackaton2015.DocumentDb/Hackaton2015.DocumentDb.Framework/AzureDocumentDbFrameworkConfigSection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton2015.DocumentDb.Framework
+{
+    public class AzureDocumentDbConfigSection : ConfigurationSection
+    {
+        // Create a "connection" element.
+        [ConfigurationProperty("connection")]
+        public ConnectionElement Connection
+        {
+            get
+            {
+                return (ConnectionElement)this["connection"];
+            }
+            set
+            { this["connection"] = value; }
+        }
+    }
+
+    // Define the "connection" element
+    // with "endpoint" and "url" attributes.
+    public class ConnectionElement : ConfigurationElement
+    {
+        [ConfigurationProperty("endpointUrl",IsRequired = true)]
+        public string EndpointUrl
+        {
+            get
+            {
+                return (String)this["endpointUrl"];
+            }
+            set
+            {
+                this["endpointUrl"] = value;
+            }
+        }
+
+        [ConfigurationProperty("key", IsRequired = true)]
+        public string Key
+        {
+            get
+            {
+                return (string)this["key"];
+            }
+            set
+            {
+                this["key"] = value;
+            }
+        }
+    }
+}
---
+++ documents_db_fwk/Hackaton2015.DocumentDb/Hackaton2015.DocumentDb.Tests/ApiDesignTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hackaton2015.DocumentDb.Framework;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hackaton2015.DocumentDb.Tests
+{
+    [TestClass]
+    public class ApiDesignTests
+    {
+        [TestMethod]
+        public void UsingAzureDbKineticaApi()
+        {
+            var store = new DocumentStoreOf<User>();
+
+            //var messages = DocumentStoreOf<User>.NewQuery()
+            //                                    .WithFilters()
+            //                                    .SortBy()
+            //                                    .WithPagingInfoLike()
+            //                                    .PerformSearchAsync()
+
+            //                      .SearchAsync(settings.Filters, 
+            //                      settings.SortColumn, 
+            //                      settings.SortAscending, 
+            //                      (pageNumber - 1) * settings.ItemsPerPage, 
+            //                      settings.ItemsPerPage);
+        }
+    }
+
+    public class DocumentStoreOf<T>
+    {
+        private DocumentClient _client;
+
+        public DocumentStoreOf()
+        {
+            try
+            {
+                var configSection = (AzureDocumentDbConfigSection)ConfigurationManager.GetSection("azureDocumentDbConfigGroup/azureDocumentDbConfig");
+                var endpointUrl = configSection.Connection.EndpointUrl;
+                var key = configSection.Connection.Key;
+                _client = new DocumentClient(new Uri(endpointUrl), key);
+            }
+            catch (Exception)
+            {
+                throw new Exception("AzureDbDocumentFramework configuration not found");
+            }
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
